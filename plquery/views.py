e0b617f50f0ce85e30df3083d5abbb05f86342e8
Fernando Colina <fernando@casapc.home.net>
2015-12-02 15:43:17 -0300

    Ahora funciona todo (sin test profundos). Faltan detalles

--- plquery/views.py
+++ plquery/views.py
@@ -55,28 +55,37 @@ class BusquedaAjaxView(generic.View):
 class BusquedaAjaxView2(generic.View):
 
     def get(self, request, *args, **kwargs):
+        # Recibe una lista con los id de los ingredientes que hay
         ingredientes_ids = request.GET.getlist('ingredientes_ids')
-
-        if 'celiaco' in request.GET and request.GET['celiaco'] == 'optSi':
-            qs_hay = Ingrediente.objects.filter(pk__in=ingredientes_ids, gluten=False)
-            qs_nohay = Ingrediente.objects.exclude(pk__in=ingredientes_ids, gluten=False)
-        else:
-            qs_hay = Ingrediente.objects.filter(pk__in=ingredientes_ids)
-            qs_nohay = Ingrediente.objects.exclude(pk__in=ingredientes_ids)
-
+        
+        # Obtengo una lista de objetos ingrediente que hay y su complemento (nohay)
+        qs_hay = Ingrediente.objects.filter(pk__in=ingredientes_ids)
+        qs_nohay = Ingrediente.objects.exclude(pk__in=ingredientes_ids)
+        # Se obtiene una lista de objetos receta que se pueden y podrian hacer con esos ingredientes
         qs_recetas = Receta.objects.filter(ingredientes__in=qs_hay).distinct()
+
+        # Si es celiaco, excluir las recetas que contengan algun ingrediente con gluten
+        if 'celiaco' in request.GET and request.GET['celiaco'] == 'optSi':
+            qs_recetas = qs_recetas.exclude(ingredientes__gluten=True)
+
+        # Filtrar las recetas por categoria
         if 'categoria_id' in request.GET and request.GET['categoria_id'] != '0':
             categoria_id = request.GET['categoria_id']
             qs_recetas = qs_recetas.filter(categoria__id=categoria_id)
 
+        # Se obtiene una lista de recetas que puedo hacer y otra lista que podria hacer
         qs_recetas_puedo = qs_recetas.exclude(ingredientes__in=qs_nohay)
         qs_recetas_podria = qs_recetas.filter(ingredientes__in=qs_nohay)
 
+        # Se genera una lista que tiene como elementos una lista con la receta que se podria hacer y los ingredientes que faltan
         list_recetas_podria = []
         for receta_podria in qs_recetas_podria:
             qs_faltan_ingredientes = qs_nohay & receta_podria.ingredientes.all()
             if len(qs_faltan_ingredientes) <= 2:
                 list_recetas_podria.append([receta_podria, qs_faltan_ingredientes])
+
+        # Se ordena la lista por longitud de los ingredientes que faltan.
+        list_recetas_podria.sort(key=lambda x: len(x[1]))
 
         html = render_to_string('plquery/resultados.html', {'recetas_list': qs_recetas_puedo,
                                                             'recetas_list_podria': list_recetas_podria})
